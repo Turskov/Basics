69f7d812d7c65790854bf4d4f2db85e06b94aa63
tsn <turskov.sn@yandex.ru>
2023-08-20 14:59:59 +0300

    CashbackHackServiceTest added

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.testng:testng:7.1.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit.jupiter:junit-jupiter-params:5.9.1"))
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-api:5.9.1")
 }
 
